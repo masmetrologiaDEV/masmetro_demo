7877e032bd72e187847bae9ad68278cc3ba4276f
masmetrologiaDEV <josecastanedauni@gmail.com>
2025-08-27 08:21:27 -0600

    ticket QRs

--- application/controllers/Compras.php
+++ application/controllers/Compras.php
@@ -315,12 +315,18 @@ class Compras extends CI_Controller {
         // Consulta base con JOIN para obtener nombre del usuario
         $query = "SELECT R.id, R.fecha, R.usuario, R.prioridad, R.tipo, R.subtipo, R.cantidad, 
                          R.cantidad_aprobada, R.unidad, R.clave_unidad, R.descripcion, R.atributos, 
-                         R.critico, R.destino, R.lugar_entrega, R.comentarios, R.estatus, R.asignado, E.nombre as nombre_empresa,
+                         R.critico, R.destino, R.lugar_entrega, R.comentarios, R.estatus, R.asignado,  COALESCE(G.empresas, '')      AS nombre_empresa,
                          CONCAT(U.nombre, ' ', U.paterno) AS User 
                   FROM requisiciones_cotizacion R 
                   LEFT JOIN usuarios U ON R.usuario = U.id 
-                  INNER JOIN qr_proveedores QP on QP.qr=R.id
-                  INNER JOIN empresas E on E.id=QP.empresa
+                  LEFT JOIN (
+        SELECT 
+            QP.qr,
+            GROUP_CONCAT(DISTINCT E.nombre ORDER BY E.nombre SEPARATOR ' / ') AS empresas
+        FROM qr_proveedores QP
+        INNER JOIN empresas E ON E.id = QP.empresa
+        GROUP BY QP.qr
+    ) G ON G.qr = R.id
                   WHERE 1 = 1";
 
         // Filtros opcionales según campos recibidos
@@ -374,7 +380,7 @@ class Compras extends CI_Controller {
                     $query .= " AND CONCAT(U.nombre, ' ', U.paterno) LIKE '%$texto%'";
                     break;
                 case 'proveedor':
-                    $query .= " AND E.nombre LIKE '%$texto%'";
+                    $query .= " AND G.empresas  LIKE '%$texto%'";
                     break;
                 case 'contenido':
                     $query .= " AND (R.descripcion LIKE '%$texto%' 
@@ -1062,7 +1068,7 @@ class Compras extends CI_Controller {
                     (SELECT MIN(fecha) FROM requisiciones_cotizacion WHERE estatus = 'COTIZANDO' AND fecha > '2021-01-01 00:00:00' $us) AS ultCotizando
                   FROM requisiciones_cotizacion 
                   WHERE fecha > '2021-01-01 00:00:00' $us";
-
+//echo $query;die();
         $res = $this->Conexion->consultar($query, TRUE);
         echo json_encode($res);
     }
